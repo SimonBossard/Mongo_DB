3882a0abe0b27387da3c7d9e3ab4b4521b4539e2
Simon <simon.bossard@etudiant.univ-rennes2.fr>
2022-04-07 17:19:47 +0200

    commit

--- Partie1.py
+++ Partie1.py
@@ -2,3 +2,14 @@ fjzfhnkjzefkjcnqs
 skdjnkcjsnd
 hdsbckbsdkjbf
 cqhbjsbck
+
+
+##test
+
+#git init
+#git remote add origin https://github.com/SimonBossard/Mongo_DB.git
+#git add partie1.py
+#git commit -a -m "test"
+
+#git push origin mgaster
+
