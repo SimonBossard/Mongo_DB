73a3f561802933e0cdef55853c60eb4e31508b0a
Simon <simon.bossard@etudiant.univ-rennes2.fr>
2022-04-07 17:39:58 +0200

    commit

--- Python/Partie1.py
+++ Python/Partie1.py
@@ -2,3 +2,23 @@
 #skdjnkcjsnd
 #hdsbckbsdkjbf
 #cqhbjsbck
+
+##test
+
+#git init
+#git remote add origin https://github.com/SimonBossard/Mongo_DB.git
+#git add partie1.py
+#git commit -a -m "test"
+
+#git push origin mgaster
+
+
+from pymongo import MongoClient
+
+db_uri = "mongodb+srv://etudiant:ur2@clusterm1.0rm7t.mongodb.net/"
+client = MongoClient(db_uri, tls=True, tlsAllowInvalidCertificates=True)
+db = client["publications"]
+print(db.list_collection_names())
+
+
+
